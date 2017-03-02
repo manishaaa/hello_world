f04894844e147c73260c05f914b15d10e33c69f4
manishaaa <memh101@gmail.com>
2017-03-02 16:09:51 -0500

    add functions

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello console")
+
+function addSum(a, b) {
+return a + b;
+}
+
+function getDifference(c,d){
+
+  return c - d;
+}
+
+var sum = addSum(3,8)
+var difference = getDifference(10,5)
+console.log(difference)
+console.log(sum)
+
+console.log(sum - difference)
+
+var name = 'Manisha'
+var greeting = 'Hello, ' + name;
+console.log(greeting)
